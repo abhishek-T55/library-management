2dcff79ade790e08080f577e415a29706825e943
abhishek-T55 <timilsina.abhishek55@gmail.com>
2024-07-05 00:26:39 +0545

    feat(models): Create models User and Book and apply changes to database.

---
+++ app/db/models/book.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, Relationship, SQLModel
+from .user import User
+
+class BookBase(SQLModel):
+    title: str = Field(min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+
+
+class BookCreate(BookBase):
+    title: str = Field(min_length=1, max_length=255)
+
+
+class BookUpdate(BookBase):
+    title: str | None = Field(default=None, min_length=1, max_length=255)
+
+
+class Book(BookBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(max_length=255)
+    owner_id: int | None = Field(default=None, foreign_key="user.id", nullable=False)
+    owner: User | None = Relationship(back_populates="books")
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,11 +1,34 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import EmailStr
 
 
-class User(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True, index=True)
-    name: str
-    phone_number: str
-    email: str
-    created_at: datetime
-    updated_at: datetime
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=200)
+    is_locked: bool = False
+    full_name: str | None = Field(default=None, max_length=100)
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=20)
+
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=20)
+    full_name: str | None = Field(default=None, max_length=200)
+
+
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(default=None, max_length=200)
+    password: str | None = Field(default=None, min_length=8, max_length=20)
+
+
+class UserUpdateMe(SQLModel):
+    full_name: str | None = Field(default=None, max_length=100)
+    email: EmailStr | None = Field(default=None, max_length=200)
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
+    # items: list["Book"] = Relationship(back_populates="owner")
