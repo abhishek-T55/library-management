453ad3984c71ab3d99cea7739d78c54febea3937
abhishek-T55 <timilsina.abhishek55@gmail.com>
2024-07-05 14:38:24 +0545

    chore(cleanup): Cleaned unwanted comments.

--- app/db/session.py
+++ app/db/session.py
@@ -4,11 +4,5 @@ from app.core.config import config
 
 engine = create_engine(str(config.database.url))
 
-
-# make sure all SQLModel models are imported (app.models) before initializing DB
-# otherwise, SQLModel might fail to initialize relationships properly
-# for more details: https://github.com/tiangolo/full-stack-fastapi-template/issues/28
-
-
 def init_db(session: Session) -> None:
     pass
